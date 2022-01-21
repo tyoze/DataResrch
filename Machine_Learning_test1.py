bef72b1c099028fa7fc45792f25d50a1fa7533ea
tyoze <timon2070@gmail.com>
2022-01-21 11:26:07 +0100

    3dimensional regression

--- Machine_Learning_test1.py
+++ Machine_Learning_test1.py
@@ -1,22 +1,19 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 Testcsv = pd.read_csv(r'test_Y3wMUE5_7gLdaTN.csv')
 Data = pd.read_csv(r'train_u6lujuX_CVtuZ9i.csv')
-Interessant = ['ApplicantIncome', 'LoanAmount']
+Interessant = ['ApplicantIncome','CoapplicantIncome', 'LoanAmount']
 
 Testdata = Testcsv[Interessant].dropna()
 data = Data[Interessant].dropna()
 
-X_train, Y_train = data['ApplicantIncome'], data['LoanAmount']
-X_test, Y_test = Testdata['ApplicantIncome'], Testdata['LoanAmount']
+X_train, Y_train, X_test, Y_test = data[['ApplicantIncome','CoapplicantIncome']], data['LoanAmount']\
+    , Testdata[['ApplicantIncome','CoapplicantIncome']], Testdata['LoanAmount']
 
-LR = LinearRegression()
-LR.fit(X_train.values.reshape(-1, 1), Y_train.values)
+lr = LinearRegression()
+lr.fit(X_train,Y_train)
 
-prediction = LR.predict(X_test.values.reshape(-1, 1))
+Y_pred = lr.predict(X_test)
 
-plt.plot(X_test, prediction)
-plt.scatter(X_test,Y_test)
-plt.show()
+print(lr.score(X_test,Y_test))
--- Machinelearning_Test2.py
+++ Machinelearning_Test2.py
@@ -1,30 +1,26 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
-from sklearn import metrics
 
-
+# import dataset and read it into pandasdataformat
 Testcsv = pd.read_csv(r'test_Y3wMUE5_7gLdaTN.csv')
 Data = pd.read_csv(r'train_u6lujuX_CVtuZ9i.csv')
 
+#choosing categories
 Interessant = ['Self_Employed','Education','Married','ApplicantIncome','CoapplicantIncome','LoanAmount']
 No_nan_data = Data[Interessant].dropna()
-X_Interessant = ['ApplicantIncome','CoapplicantIncome']
+X_Interessant = ['ApplicantIncome']
 test_data = Data[Interessant].dropna()
 
+#deploying chosen catagories on dataset
 X = No_nan_data[X_Interessant]
 Y = No_nan_data['LoanAmount']
 X_test = test_data[X_Interessant]
 Y_test = test_data['LoanAmount']
 
-clf = DecisionTreeClassifier()
-clf = clf.fit(X,Y)
-prediction = clf.predict(X_test)
+#decision tree model
+clf = tree.DecisionTreeClassifier()
+clf.fit(X,Y)
 
-fig = plt.figure(figsize=(25,20))
-_ = tree.plot_tree(clf,
-                   feature_names=X_Interessant,
-                   filled=True)
-plt.show()
-
+#visual representation of the tree
+clf.max_depth()
