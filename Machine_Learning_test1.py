697df8c3de00d32830dbe15897e3ad192eaea5e8
tyoze <timon2070@gmail.com>
2022-01-28 21:56:49 +0100

    extra variables in model

--- Machine_Learning_test1.py
+++ Machine_Learning_test1.py
@@ -3,8 +3,8 @@ from sklearn.linear_model import LinearRegression
 
 Testcsv = pd.read_csv(r'test_Y3wMUE5_7gLdaTN.csv')
 Data = pd.read_csv(r'train_u6lujuX_CVtuZ9i.csv')
-Nodig = ['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Education', 'Self_Employed', 'Married']
-Intressant = ['ApplicantIncome', 'CoapplicantIncome', 'Education', 'Self_Employed', 'Married']
+Nodig = ['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Education', 'Self_Employed', 'Married','Gender']
+Intressant = ['ApplicantIncome', 'CoapplicantIncome', 'Education', 'Self_Employed', 'Married','Gender']
 
 Testdata = Testcsv[Nodig].dropna()
 data = Data[Nodig].dropna()
