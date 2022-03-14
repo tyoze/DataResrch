827fbc4804f2965d9e4fb75602aa11cf022604b7
tyoze <timon2070@gmail.com>
2022-03-14 18:57:05 +0100

    DecTreeClassifier to DecTreeRegression

--- Machinelearning_Test2.py
+++ Machinelearning_Test2.py
@@ -1,8 +1,9 @@
 import pandas as pd
+import numpy
 import matplotlib.pyplot as plt
 import sklearn.tree
 from sklearn import tree
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error, r2_score
 
 # import dataset and read it into pandasdataformat
 Testcsv = pd.read_csv(r'test_Y3wMUE5_7gLdaTN.csv')
@@ -23,16 +24,18 @@ test_data.replace(to_replace=['Yes', 'No'], value=[1, 0], inplace=True)
 test_data.replace(to_replace=['Graduate', 'Not Graduate'], value=[1, 0], inplace=True)
 
 #deploying chosen catagories on dataset
+
 X = No_nan_data[X_Interessant]
 Y = No_nan_data['LoanAmount']
 X_test = test_data[X_Interessant]
 Y_test = test_data['LoanAmount']
 
 #decision tree model
-clf = tree.DecisionTreeClassifier()
-clf.fit(X,Y)
+clf = tree.DecisionTreeRegressor(max_depth=4).fit(X,Y)
 prediction = clf.predict(X_test)
-print(accuracy_score(prediction,Y_test))
+mse = mean_squared_error(Y_test, prediction)
+r2 = r2_score(Y_test,prediction)
+print(mse,r2)
 
 #visual representation of the tree
 sklearn.tree.plot_tree(clf)
